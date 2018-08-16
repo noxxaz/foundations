0d668ce9a09b79781f0bdab826b088a55fcb9612
Zach Brown <zbrown@pivotal.io>
2018-08-16 19:03:52 +0900

    class warmup

--- week-01/4-thursday/script.js
+++ week-01/4-thursday/script.js
@@ -1,1 +1,15 @@
 console.log("Welcome to Code Chrysalis Foundations!");
+
+function greeting(name){
+    return "Hello " + name + "!!!!"
+}
+
+function average(num1, num2) {
+    return (num1 + num2) / 2
+}
+
+console.log(greeting("Mike"));
+console.log(greeting("Beau"));
+
+console.log(average(29348,.79));
+console.log(average(100,1));
