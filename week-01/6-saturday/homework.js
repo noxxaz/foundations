c55abed3419e9610335ed6a84254c474e3b01c24
Zach Brown <zbrown@pivotal.io>
2018-08-20 23:07:08 +0900

    finished homework

--- week-01/6-saturday/homework.js
+++ week-01/6-saturday/homework.js
@@ -33,3 +33,24 @@ function factorial(n) {
 // Test cases
 console.log(factorial(4)); // 4 * 3 * 2 * 1 ---> 24
 console.log(factorial(5)); // 5 * 4 * 3 * 2 * 1 ---> 120
+
+// Write a function called isEven that, given a number n as a parameter, returns true if that 
+//number is even, and false otherwise; however, you need to do this without using the % operator.
+function isEven(n) {
+    return Number.isInteger(n / 2);
+}
+//Tests
+console.log(isEven(2));
+console.log(isEven(20));
+console.log(isEven(205));
+
+
+// Write a function validCredentials that accepts two parameters: username and password, and 
+// returns true if both are long enough, and false otherwise. You can decide what constitutes "long enough".
+function validCredentials(username, password) {
+    return (username.length >= 8 && password.length >= 8);
+}
+
+console.log(validCredentials("yo","mama"));
+console.log(validCredentials("fritolays","1potato2"));
+console.log(validCredentials("chunkalicious","supercalifragilistic"));
