c25d7b7be91b4a073def4431fd92359e74c39612
Zach Brown <zbrown@pivotal.io>
2018-08-20 23:25:23 +0900

    fizzBuzz

--- week-01/6-saturday/homework.js
+++ week-01/6-saturday/homework.js
@@ -54,3 +54,32 @@ function validCredentials(username, password) {
 console.log(validCredentials("yo","mama"));
 console.log(validCredentials("fritolays","1potato2"));
 console.log(validCredentials("chunkalicious","supercalifragilistic"));
+
+
+
+// Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+// For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 
+// (and not 3), print "Buzz" instead.
+
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible 
+// by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+function fizzBuzz() {
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            // divisible by both 3 & 5
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            //divisible only by 3
+                console.log("Fizz");        
+            } else if (i % 5 === 0) {
+            //divisible only by 5
+                console.log("Buzz");
+        } else {
+            //not divisible by 3 or 5
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz();
