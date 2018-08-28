ab9928d9a4b4e4b72472af08f502268e4617b410
Zach Brown <zbrown@pivotal.io>
2018-08-28 19:11:01 +0900

    warmup

--- week-03/2-tuesday/script.js
+++ week-03/2-tuesday/script.js
@@ -1,2 +1,20 @@
 "use strict"
 console.log("Welcome to Code Chrysalis Foundations!");
+
+// WARMUP: Declare a function countCharacters that, when given a string as an argument, 
+// returns an object containing counts of the occurrences of each character in the string
+// example: Mississippi
+
+console.log(countCharacters("Mississippi"));
+// {M: 1, i: 4, s: 4, p: 2}
+
+function countCharacters(someStr) {
+    let result = {};
+    let char = "";
+    for (let i = 0; i < someStr.length; i++) {
+        char = someStr.slice(i, i + 1);
+        if (!result[char]) { result[char] = 0; }
+        result[char]++;
+    }
+    return result;
+}
