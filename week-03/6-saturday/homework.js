6016ee75ec8a550bc567547a75c2c4871bc7c6a3
Zach Brown <zbrown@pivotal.io>
2018-09-02 23:00:01 +0900

    homework done

--- week-03/6-saturday/homework.js
+++ week-03/6-saturday/homework.js
@@ -52,8 +52,48 @@ console.log(indexedExponentials([2, 5, 7, 4])); // => [2^0, 5^1, 7^2, 4^3] => [1
 // even indices.
 
 function evenIndexedOddNumbers(numbers) {
-  // TODO: Your code here
+  const result = [];
+  each1(numbers, function(number, index) {
+    if (index % 2 === 0 && number % 2 === 1) {
+        result.push(number);
+    }
+  });
+  return result;
 }
-evenIndexedOddNumbers([1, 3, 3, 4, 7, 10]); // => [1, 3, 7]
+console.log(evenIndexedOddNumbers([1, 3, 3, 4, 7, 10])); // => [1, 3, 7]
 
 
+// Write a function called evenIndexedEvenLengths (yeah, it is long) that
+// accepts an array of strings as a parameter, and returns only the strings
+// that are found at an even index that also have an even length, 
+// e.g.:
+
+function evenIndexedEvenLengths(strings) {
+  const result = [];
+  each1(strings, function(string, index) {
+    if (index % 2 === 0 && string.length % 2 === 0) {
+      result.push(string);
+    }
+  });
+  return result;
+}
+
+console.log(evenIndexedEvenLengths(["lion", "monkey", "aardvaark", "cat", "doge"]));
+// => ["lion", "doge"]
+console.log(evenIndexedEvenLengths(["red", "green", "purple", "blue", "yellow"]));
+// => ["purple", "yellow"]
+
+
+// Write a function sumByAllElementsMultipliedByFour that takes an array as an
+// argument and returns the sum of all elements multiplied by four.
+
+function sumByAllElementsMultipliedByFour(numArray) {
+  let result = 0;
+  each1(numArray, function(number) { // ignore the index arg in the callback
+    result += number * 4;
+  });
+  return result;
+}
+
+console.log(sumByAllElementsMultipliedByFour([1, 2, 3, 4, 5, 6])); 
+// => 84
